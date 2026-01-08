3e93bca306b74c64ed05e1465ab57e2ee791f18e
Amadeus Kaczmarek <141212854+amadeusk1@users.noreply.github.com>
2026-01-07 22:46:21 -0700

    newer build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.amadeusk.liftlog"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
