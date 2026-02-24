d41bb3566c06bd4745cedb6e753b0eb546667e60
Amadeus Kaczmarek <141212854+amadeusk1@users.noreply.github.com>
2026-02-24 13:44:47 -0700

    build number change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.amadeusk.liftlog"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.0"
+        versionCode = 11
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
